ff1f45f78919cc9c39b37e20414e8f72a87b9365
varshithaiosys <110538230+varshithaiosys@users.noreply.github.com>
2023-01-23 12:11:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+/* ----- Menu js _------*/
+function onClickMenu(){
+    document.getElementById("menu").classList.toggle("icon");
+    document.getElementById("nav").classList.toggle("change");
+}
+
+/* ----- Menu js ends here_------*/
+
+/* -----  video popup js  starts here------*/
+$('.tourist-slider .cta-two a').fancybox({
+    caption: function (instance, item) { }
+});
+
+/* -----  video popup js ends  here------*/
+
+/* ----- Slider video popup js  starts here------*/
+$('.two-col-image-text .icon a').fancybox({
+    caption: function (instance, item) { }
+});
+/* ----- Slider video popup js  starts here------*/
+
